fd7371eb19309a081fa0a7d52bd10f24379914a9
AlexTaures <alextauresdiaz@gmail.com>
2023-05-15 10:49:46 -0600

    update

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # NodeJsTemplate
 
+
+Versions: 
+  Node: 18.16.0
+  npm: 9.5.1
+  js: 12.22.9
 npm install
--- app.js
+++ app.js
@@ -1,18 +1,32 @@
 const express = require('express');
+const hbs = require('hbs');
 const app = express();
 const port= 8080; 
 
+//para uso de partials
 //MiddleWare para ejecutar contenido dentro de la carpeta public
-app.use(express.static('public'));
+// app.use(express.static('public')); //Sin comentar esto, se crea conflico con el el hbs
+//////////
+
+//Handlebars
+//TODO: require('hbs'); buscara por defecto en la carpeta views
+app.set('view engine', 'hbs');
+hbs.registerPartials(__dirname + '/views/partials', function (err) {
+  console.log("error: " + err +"\nin "+ __dirname);
+});
 
 
 app.get('/', function (req, res) {
-  res.send('Home page')
-})
+  //res.send('Home page');
+  res.render('home',{
+    nombre: 'Alex Taures',
+    titulo: 'template'
+  });
+}) 
 
-app.get('/hello', function (req, res) {
-  res.send('Hello Word')
-})
+// app.get('/hello', function (req, res) {
+//   res.send('Hello Word');
+// })
 app.get('*', function (req, res) {
   res.sendFile(__dirname + 'public/404.html');
 })
